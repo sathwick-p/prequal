be0873a9f7bf22e0c4ca0047e36779c2e99672f2
sathwick.p <sathwick.p7@gmail.com>
2026-02-08 22:32:14 +0530

    small changes

--- loadbalancer/prequal/prequal.go
+++ loadbalancer/prequal/prequal.go
@@ -1,1 +1,16 @@
 package prequal
+
+import (
+	"fmt"
+	"time"
+)
+
+
+type Stats struct {
+	inflight int64
+	ewmaLatency time.Duration
+	lastUpdated time.Time
+} 
+func GetEndpointKey(routeKey, addr string, port int32) string {
+	return fmt.Sprintf("%s|%s:%d", routeKey, addr, port)
+}
